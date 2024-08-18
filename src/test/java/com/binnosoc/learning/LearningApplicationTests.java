a13519c9b2df57f6142926fe4a2ea977876f4023
Boro Ousseni <bin.innosoc@gmail.com>
2024-08-18 16:56:37 +0100

    first commit

---
+++ src/test/java/com/binnosoc/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binnosoc.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
